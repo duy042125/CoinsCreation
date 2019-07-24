cce19719adf753160b75e20af8739c47608dfb97
Duy Nguyen <dnguye14@uwsuper.edu>
2019-07-23 22:12:17 -0500

    finishing connect the interface of ReportShop and Database

--- CreativeCoin/Interface/ReportShop.xaml.cs
+++ CreativeCoin/Interface/ReportShop.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Middleware;
+using Middleware.Database_Component;
 
 namespace Interface
 {
@@ -26,12 +28,34 @@ namespace Interface
 
         private void Information_Loaded(object sender, RoutedEventArgs e)
         {
+            #region Report Load
+            Child childLoader = DBConnection.retrieveChildByName(LogInInformation.Username, LogInInformation.Child_name);
+            ChildName.Text = childLoader.Child_name;
+            Age.Text = DateTimeConverter.timeSpanToString(DateTime.Now - childLoader.birthdate);
+            Date.Text = DateTime.Now.ToString("MM/dd/yyyy");
+            TotalCoin.Content = childLoader.total_coin;
+            #endregion
 
+            #region Reward Load
+            Behavior behaviorLoader = DBConnection.retrieveBehaviorByName(LogInInformation.Behavior_name);
+            Reward51.Content = behaviorLoader.star5_reward1;
+            Reward52.Content = behaviorLoader.star5_reward2;
+            Reward53.Content = behaviorLoader.star5_reward3;
+
+            Reward101.Content = behaviorLoader.star5_reward1;
+            Reward102.Content = behaviorLoader.star5_reward2;
+            Reward103.Content = behaviorLoader.star5_reward3;
+
+            Reward151.Content = behaviorLoader.star5_reward1;
+            Reward152.Content = behaviorLoader.star5_reward2;
+
+            Reward20.Content = behaviorLoader.star20_reward;
+            #endregion
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-
+            Note.Text = "";
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
